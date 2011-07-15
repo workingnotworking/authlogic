5b243c574578a24dc6cd8c82ac93bac2fea9d982
Surendra Singhi <ssinghi@kreeti.com>
2011-07-15 18:07:30 +0530

    fix authlogic issue #267

--- lib/authlogic/session/http_auth.rb
+++ lib/authlogic/session/http_auth.rb
@@ -11,10 +11,10 @@ module Authlogic
         klass.class_eval do
           extend Config
           include InstanceMethods
-          persist :persist_by_http_auth, :if => :persist_by_http_auth?
+          persist :persist_by_http_auth
         end
       end
-      
+
       # Configuration for the HTTP basic auth feature of Authlogic.
       module Config
         # Do you want to allow your users to log in via HTTP basic auth?
@@ -64,15 +64,16 @@ module Authlogic
         end
         alias_method :http_basic_auth_realm=, :http_basic_auth_realm
       end
-      
+
       # Instance methods for the HTTP basic auth feature of authlogic.
       module InstanceMethods
         private
           def persist_by_http_auth?
             allow_http_basic_auth? && login_field && password_field
           end
-        
+
           def persist_by_http_auth
+            return false unless persist_by_http_auth?
             login_proc = Proc.new do |login, password|
               if !login.blank? && !password.blank?
                 send("#{login_field}=", login)
@@ -86,10 +87,10 @@ module Authlogic
             else
               controller.authenticate_with_http_basic(&login_proc)
             end
-        
+
             false
           end
-        
+
           def allow_http_basic_auth?
             self.class.allow_http_basic_auth == true
           end
